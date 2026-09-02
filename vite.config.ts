3424b8b919facec36102da9531acac6fa1ddbf30
angellzz2 <victorbertie201911@yahoo.com>
2026-09-02 14:52:50 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "34514e38-d1ce-4343-bfff-26b0dcf8f4e0";
 
 const { d1, r2 } = hostingConfig;
 
